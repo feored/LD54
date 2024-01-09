bd2fe1f45800e5745f17b19a1ec65abbf055fbbe
feored <feordo@gmail.com>
2024-01-10 00:26:48 +0100

    Add buildings to random map generation.

--- scenes/map_generator/map_generator.gd
+++ scenes/map_generator/map_generator.gd
@@ -9,6 +9,14 @@ var teams = []
 var selected_team_num = 7
 var island_size = Constants.ISLAND_SIZE_DEFAULT
 
+const BUILDING_PROBABILITIES = {
+	Constants.Building.Oracle: 0.2,
+	Constants.Building.Temple: 0.2,
+	Constants.Building.Seal: 0.1,
+	Constants.Building.Fort: 0.1,
+	Constants.Building.Barracks: 0.4,
+}
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -37,6 +45,7 @@ func add_teams():
 func gen_world():
 	await self.world.clear_island()
 	await self.world.generate_island(island_size, Settings.get_setting(Settings.Setting.InstantMap))
+	self.gen_buildings()
 	self.add_teams()
 
 
@@ -65,3 +74,19 @@ func _on_island_size_slider_drag_ended(value_changed):
 
 func _on_instant_button_toggled(button_pressed:bool):
 	Settings.set_setting(Settings.Setting.InstantMap, button_pressed)
+
+func gen_buildings():
+	var nb_tiles = self.world.tiles.size() / 15
+	var random_tiles = self.world.tiles.values()
+	random_tiles.shuffle()
+	for i in range(nb_tiles):
+		var random_building = Utils.rng.randf()
+		var total = 0
+		for b in BUILDING_PROBABILITIES:
+			total += BUILDING_PROBABILITIES[b]
+			if random_building < total:
+				random_building = b
+				break
+		random_tiles[i].set_building(random_building)
+		random_tiles[i].update()
+	
