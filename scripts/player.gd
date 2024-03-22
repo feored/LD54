8de9d0506983bd84da8580a96f6c7181fce8e5fd
feored <feordo@gmail.com>
2024-03-22 20:34:28 +0100

    Move turn + player info into separate scripts

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ class_name Player
 
 var team: int = 0
 var eliminated: bool = false
-var resources: Dictionary = {}
+var resources: Dictionary = {"faith": 0}
 var cards_in_use: Array = []
 var is_bot: bool = false
 var bot: Bot = null
--- scripts/turnmanager.gd
+++
@@ -1,7 +0,0 @@
-extends RefCounted
-class_name TurnManager
-
-var turn: int = 0
-var global_turn: int = 0
-var players: Array = []
-var current_player: Player
